c3f27bdde2e1fc45b66743fd301a84533710f35d
alan chang <chang_a@qwasar.io>
2023-11-14 22:17:46 +0000

    matrix complete

--- src/helpers.c
+++ src/helpers.c
@@ -7,3 +7,20 @@ int check_arg_ct(int ac){
     }
     return 0;
 }
+
+int get_line_ct(int fd){
+    char* first_line = my_readline(fd);
+    int num = 0;
+    while (*first_line >= '0' && *first_line <= '9'){
+        num = num * 10 + (*first_line - '0');
+        first_line++;
+    }
+    return num;
+}
+
+int my_min(int a, int b, int c){
+    int min = a;
+    if (b < min) min = b;
+    if (c < min) min = c;
+    return min;
+}
--- src/my_bsq.c
+++ src/my_bsq.c
@@ -22,15 +22,47 @@ int main(int ac, char** av){
 
     if(check_arg_ct(ac) != 0) return -1;
 
-    char *str = NULL;
+    
     int fd = open(av[1], O_RDONLY);
-    while ((str = my_readline(fd)) != NULL)
-    {
-        printf("%s\n", str);
-        free(str);
+
+    // read first line, store as integer
+    int line_ct = get_line_ct(fd);
+    printf("The number is: %d\n", line_ct); // TEST PRINT
+
+    // create matrix
+    int matrix[line_ct][line_ct];
+
+    // first row of the map
+    char *first_row = my_readline(fd);
+    for (int i = 0; i < line_ct; i++){
+        if (first_row[i] == '.') matrix[0][i] = 1;
+        if (first_row[i] == 'o') matrix[0][i] = 0;
     }
 
-    close(fd);
+    // the rest of the map
+    char *row = NULL;
+    int row_index = 1;
+    while ((row = my_readline(fd)) != NULL){
+        // first character
+        if (row[0] == '.') matrix[row_index][0] = 1;
+        if (row[0] == 'o') matrix[row_index][0] = 0;
+        // rest of the characters
+        for (int i = 1; i < line_ct; i++){
+            if (row[i] == 'o') matrix[row_index][i] = 0;
+            else matrix[row_index][i] = my_min(matrix[row_index][i - 1], matrix[row_index - 1][i], matrix[row_index - 1][i - 1]) + 1;
+        }
+        row_index++;
+    }
+
+    for (int i = 0; i < line_ct; i++){
+        for (int j = 0; j < line_ct; j++){
+            printf("%d", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
 
     return 0;
 }
